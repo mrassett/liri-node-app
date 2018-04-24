d160dc1db88b211b1563c32f26c4e10b66ea4e48
mrassett <magsrassett@gmail.com>
2018-04-24 00:00:42 -0500

    twitter and spotify

--- liri.js
+++ liri.js
@@ -16,50 +16,35 @@ let params = {
   mrass2018: 'nodejs',
   count: 20
 };
-
-
+let song = process.argv[3];
 let nodeArgs = process.argv;
 console.log(process.argv);
 let commandLinearg = nodeArgs[2];
 
-if (commandLinearg === 'my-tweets') {
-  tweetOutput = twentyTweets()
-  // console.log(tweetOutput);
-}
-else if (commandLinearg === 'spotify-this-song') {
-  spotifyOutput = spotSong()
-  console.log(spotifyOutput);
-}
-else if (commandLinearg === 'movie-this') {
-  movieOutput = movieThis()
-  console.log(movieOutput);
-}
-else if (commandLinearg === 'do-what-it-says') {
-  otherOutput = doIt()
-  console.log(otherOutput);
-}
-
 
 //Spotify
+function spotSong (song) {
+  spotify.search ({ type: 'track', query: song, limit: 5}, function (err, data) {
 
-// function spotSong (songName) {
-//   spotify.search ({
-//     type: 'track',
-//     query: songName
-//     }, function (err, data) {
-//     if (err) {
-//       return console.log('Error occurred: ' + err);
-//     }
-//     console.log(data);
-//   })
-// }
+    if (!error) {
+      let songInfo = data.tracks.items[0];
+      console.log(data)
+    }
+    else if (error){
+    return console.log('Error occurred: ');
+    }
+  })
+}
+// .tracks.items[0]
 
-function twentyTweets() {
 //Twitter
-//wrap around a function (call using Node)
+function twentyTweets() {
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
   if (!error) {
-    console.log(tweets);
+    for (var i = 0; i < tweets.length; i++){
+      let a = tweets[i].user.created_at + '\n' + tweets[i].text;
+      console.log(a);
+    }
   } if (error) {
     return console.log('Error occurred');
   }
@@ -67,6 +52,7 @@ function twentyTweets() {
 
 
 // OMDB
+function movieThis(){
 for (var i = 2; i < nodeArgs.length; i++) {
   if (i > 2 && i < nodeArgs.length) {
     movieName = movieName + "+" + nodeArgs[i];
@@ -81,7 +67,7 @@ let queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey
 //used inside OMDB call (OMDB doesn't have this built in, like the others)
 //replace google with OMDB URL, build out the same way that an AJAX request is 
 request(queryUrl, function (error, response, body) {
-  if (!error && response.statusCode === 200) {
+  if (!error && response.statusCode === 200 && commandLinearg === 'movie-this' + movieName) {
   console.log(queryUrl);
   console.log('error:', error); // Print the error if one occurred
   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
@@ -92,3 +78,16 @@ request(queryUrl, function (error, response, body) {
     });
 
   }
+}
+if (commandLinearg === 'my-tweets') {
+  twentyTweets()
+  // console.log(tweetOutput);
+}
+else if (commandLinearg === 'spotify-this-song' + song) {
+ return spotSong()
+  // console.log(spotifyOutput);
+}
+else if (commandLinearg === 'do-what-it-says') {
+   return doIt()
+  
+}
